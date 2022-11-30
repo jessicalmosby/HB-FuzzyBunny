224067525bd54760ad165324035fb9e1201e0e0b
Jessica Mosby <jessicamosby@mac.com>
2022-11-30 16:03:30 -0500

    added eventListeners for the form and window load

--- create/create.js
+++ create/create.js
@@ -5,10 +5,19 @@ const logoutButton = document.getElementById('logout');
 
 form.addEventListener('submit', async (e) => {
     // prevent default
+    e.preventDefault();
 
     // get the name and family id from the form
+    const formData = new FormData(form);
+
+    const familyId = formData.get('family-id');
+    const name = formData.get('bunny-name');
 
     // use createBunny to create a bunny with this name and family id
+    await createBunny({
+        name: name,
+        familyId: familyId,
+    });
 
     form.reset();
 });
@@ -16,11 +25,19 @@ form.addEventListener('submit', async (e) => {
 window.addEventListener('load', async () => {
     // let's dynamically fill in the families dropdown from supabase
     // grab the select HTML element from the DOM
+    const select = document.querySelector('select');
     // go get the families from supabase
+    const families = await getFamilies();
     // for each family
     // create an option tag
-    // set the option's value and text content
-    // and append the option to the select
+    for (let family of families) {
+        const option = document.createElement('option');
+        // set the option's value and text content
+        option.value = family.id;
+        option.textContent = family.name;
+        // and append the option to the select
+        select.append(option);
+    }
 });
 
 checkAuth();
