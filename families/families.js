632027bd8ff8af4db5d8abf729ab60658ea3cec4
Jessica Mosby <jessicamosby@mac.com>
2022-11-30 15:47:02 -0500

    created displayFamilies function in families.js

--- families/families.js
+++ families/families.js
@@ -9,9 +9,11 @@ logoutButton.addEventListener('click', () => {
     logout();
 });
 
-function displayFamilies() {
+function displayFamilies(families) {
     // fetch families from supabase
+    getFamilies();
     // clear out the familiesEl
+    familiesEl.textContent = '';
     // loop through each family and for each family:
     // create three elements for each family, one for the whole family, one to hold the name, and one to hold the bunnies
     // your HTML Element should look like this:
@@ -22,14 +24,42 @@ function displayFamilies() {
     //        <div class="bunny">Bob</div>
     //    </div>
     // </div>
-    // add the bunnies css class to the bunnies el, and family css class to the family el
-    // put the family name in the name element
-    // for each of this family's bunnies
-    //    make an element with the css class 'bunny', and put the bunny's name in the text content
-    //    add an event listener to the bunny el. On click, delete the bunny, then refetch and redisplay all families.
-    // append this bunnyEl to the bunniesEl
-    // append the bunniesEl and nameEl to the familyEl
-    // append the familyEl to the familiesEl
+    for (let family of families) {
+        const familyEl = document.createElement('div');
+        const nameEl = document.createElement('h3');
+        const bunniesEl = document.createElement('div');
+
+        // add the bunnies css class to the bunnies el, and family css class to the family el
+        bunniesEl.classList.add('bunnies');
+        familyEl.classList.add('family');
+
+        // put the family name in the name element
+        nameEl.textContent = family.name;
+
+        for (let bunny of family.fuzzy_bunnies) {
+            const bunnyEl = document.createElement('div');
+
+            // for each of this family's bunnies
+            //    make an element with the css class 'bunny', and put the bunny's name in the text content
+            bunnyEl.classList.add('bunny');
+            bunnyEl.textContent = bunny.name;
+
+            //    add an event listener to the bunny el. On click, delete the bunny, then refetch and redisplay all families.
+            bunnyEl.addEventListener('click', async () => {
+                await deleteBunny(bunny.id);
+
+                const updatedFamilies = await getFamilies();
+
+                displayFamilies(updatedFamilies);
+            });
+            // append this bunnyEl to the bunniesEl
+            bunniesEl.append(bunnyEl);
+        }
+        // append the bunniesEl and nameEl to the familyEl
+        familyEl.append(nameEl, bunniesEl);
+        // append the familyEl to the familiesEl
+        familiesEl.append(familyEl);
+    }
 }
 
 window.addEventListener('load', async () => {
